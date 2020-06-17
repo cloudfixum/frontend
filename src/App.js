dc7425406e378f0baaacfd4429a126d39273c20b
Matias Ahumada <ahumadamatias7@gmail.com>
2020-06-17 02:58:09 -0300

    first commit, setup ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../src/assets/styles/style.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proximamente CloudFixum</h1>
+      <p>Proyecto de Programacion II</p>
+    </div>
+  );
+}
+
+export default App;
