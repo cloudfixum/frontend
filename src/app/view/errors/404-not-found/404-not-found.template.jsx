c86df0fc6595979e822f70809ce96cff8be4dee8
FacuLuna745 <lunafacundo03@gmail.com>
2020-08-19 18:49:45 -0300

    Create 404 view

---
+++ src/app/view/errors/404-not-found/404-not-found.template.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './404-not-found.scss'
+export default (props) => {
+    return(
+        <div className="container-not-found">
+            
+        </div>
+    );
+}
