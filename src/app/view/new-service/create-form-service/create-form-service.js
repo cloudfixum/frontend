1ba865772b2f443cb2a95ff9d27055dd8f99f1e4
Matias Ahumada <ahumadamatias7@gmail.com>
2020-08-20 21:56:56 -0300

    prettier fxs

--- src/app/view/new-service/create-form-service/create-form-service.js
+++ src/app/view/new-service/create-form-service/create-form-service.js
@@ -1,34 +1,34 @@
 import template from './create-form-service.template';
 
 import { useState } from 'react';
-import ServicesApi from "../../../shared/services/services-api";
+import ServicesApi from '../../../shared/services/services-api';
 
 export default function CreateFormServices() {
-
     const values = {
         title: '',
         category: '',
         description: '',
         base_price: 0,
-    }
+    };
 
-    const [valuesForm, setValuesForm] = useState(values)
+    const [valuesForm, setValuesForm] = useState(values);
 
     const createService = (e) => {
-        (new ServicesApi()).addService(valuesForm)
-            .then(res => {
-                console.log(res)
+        new ServicesApi()
+            .addService(valuesForm)
+            .then((res) => {
+                console.log(res);
             })
-            .catch(e => {
-                console.log(e)
-            })
-    }
+            .catch((e) => {
+                console.log(e);
+            });
+    };
 
     const props = {
         valuesForm,
         setValuesForm,
         createService,
-    }
+    };
 
     return template(props);
 }
