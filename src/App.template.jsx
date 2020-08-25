7043c541f5f116bfc278787d9f16a41811778e35
Matias <ma.navarro@alumno.um.edu.ar>
2020-08-25 19:28:51 -0300

    fix warnings and deleted useless route

--- src/App.template.jsx
+++ src/App.template.jsx
@@ -3,9 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './app/view/home-page/home-page';
 import NewService from './app/view/new-service/new-service';
 import NavBar from './app/shared/components/nav-bar/nav-bar';
-import PaginationTest from './app/view/home-page/layout-card-services/layout-card-services.template'
-
-import { makeStyles } from '@material-ui/core/styles';
 
 import '../src/assets/styles/style.scss';
 import './App.scss'
@@ -19,7 +16,6 @@ export default (props) => {
                 <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route exact path="/new-service" component={NewService} />
-                    <Route exact path="/pagination" component={PaginationTest} />
                     <Route path ="/*" component={NotFound} />
                 </Switch>
             </BrowserRouter>
