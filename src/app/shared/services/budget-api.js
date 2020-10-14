4fec6fcea380bd1951335faa8309b2905a24acde
Matias Ahumada <ahumadamatias7@gmail.com>
2020-10-14 15:32:36 -0300

    autocommit

--- src/app/shared/services/budget-api.js
+++ src/app/shared/services/budget-api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE = 'https://cloudfixum-api-dev.herokuapp.com/api/';
+const BASE = 'https://cloudfixum-api.herokuapp.com/api/';
 
 class BudgetApi {
     async createBudgetRequest(budgetRequest) {
